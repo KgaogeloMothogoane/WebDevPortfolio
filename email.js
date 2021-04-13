ff033f44a1098dab22ba6205fbf389fcfa4f2f82
Kgaogelo Mothogoane <kgaogelomothogoane@gmail.com>
2021-04-13 03:07:04 +0200

    Mobile view updated

---
+++ email.js
@@ -0,0 +1,29 @@
+function sendMail(params) {
+  var templateParams = {
+    userName: document.getElementsByName("userName").value,
+    userNumber: document.getElementsByName("userNumber").value,
+    userEmail: document.getElementsByName("userEmail").value,
+    userMessage: document.getElementsByName("userMessage").value,
+  };
+
+  const btn = document.getElementById("button");
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    btn.value = "Sending...";
+
+    const serviceID = "default_service";
+    const templateID = "template_4ti5qmm";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Send Email";
+        alert("Sent!");
+      },
+      (err) => {
+        btn.value = "Send Email";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
+}
